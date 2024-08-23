8d745795d12922170682511a341eb2ee6856c221
DeeeJaaay <46990256+DeeeJaaay@users.noreply.github.com>
2024-08-23 08:32:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,15 @@ pickButton.addEventListener("click", () => {
     const randomRestaurant = restaurants[randomIndex];
     resultElement.textContent = `Ni borde prova ${randomRestaurant}!`;
 });
+
+// Theme switching code
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('switchButton').addEventListener('click', function() {
+        var themeStylesheet = document.getElementById('themeStylesheet');
+        if (themeStylesheet.getAttribute('href') === 'styles.css') {
+            themeStylesheet.setAttribute('href', 'styles2.css');
+        } else {
+            themeStylesheet.setAttribute('href', 'styles.css');
+        }
+    });
+});
